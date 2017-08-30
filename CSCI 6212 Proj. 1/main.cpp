4bd18125123b88a48207dbd5aeb6ef4e429d641d
Photon Cat <lkstc112233@gmail.com>
2017-08-29 23:03:25 -0400

    Initial Commit

---
+++ CSCI 6212 Proj. 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI 6212 Proj. 1
+//
+//  Created by 煦 柯 on 2017/8/29.
+//  Copyright © 2017年 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
