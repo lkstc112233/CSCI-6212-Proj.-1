70e41a3bc5ad89af23b9705b8c979edeeafedc2c
Photon Cat <lkstc112233@gmail.com>
2017-08-29 23:29:11 -0400

    Benchmark part

--- CSCI 6212 Proj. 1/main.cpp
+++ CSCI 6212 Proj. 1/main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <utility>
+#include <vector>
+#include <chrono>
 
 int gcd(int n, int m)
 {
@@ -19,9 +21,30 @@ int gcd(int n, int m)
     return gcd(m, r);
 }
 
-
+// Run the Option 3 with size n, outputing the time elapsed in nanoseconds.
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 100000;
+    if (argc > 1)
+        n = atoi(argv[1]);
+    if (n == 0)
+        n = 1000;
+    // creates some n-item arrays, filling with 0.
+    std::vector<int> a(n, 0);
+    std::vector<int> b(n, 0);
+    std::vector<int> c(n, 0);
+    int Sum = 0;
+    // benchmark
+    auto begin = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i <= n; ++i)
+        for (int j = i; j <= n; ++j)
+        {
+            for (int k = j; k <= n; ++k)
+                Sum += a[i] * b[j] * c[k];
+            if (gcd(i,j) == 1)
+                j = n;
+        }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << std::endl;
     return 0;
 }
+
